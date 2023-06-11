f3eaabf0638f1138a6fc47b170562b1fc1abc09d
Sricharan <83802694+Korpse614@users.noreply.github.com>
2023-06-11 13:14:50 +0530

    Add files via upload

---
+++ L12_Potentiometer/L12_Potentiometer.ino
@@ -0,0 +1,17 @@
+int VoltPin = A2;
+int readVal;
+float V2;
+int time = 500;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  readVal = analogRead(VoltPin);
+  V2 = (5./1023.)*readVal;
+  Serial.println(V2);
+  delay(time);
+  }
